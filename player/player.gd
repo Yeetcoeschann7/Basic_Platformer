2a910a93017d38a5a30edbaef384b8ee742a6769
Jacob L <jlewis.cw743@gmail.com>
2023-09-18 13:20:24 -0400

    Small Changes
    - Fixed being able to walljump from ground
    - Changed player sprite to slime
    - Added some sfx and squish animation

--- player/player.gd
+++ player/player.gd
@@ -13,17 +13,20 @@ var jumpReset = false
 var buffer_frames = 0
 var is_sliding = false
 var no_move = ["left", "right"]
+var next_land = false
+var sec = 0
 
 
 
 func _physics_process(delta):
-	$ray_left/particles_left.emitting = false
-	$ray_right/particles_right.emitting = false
+	if sec < 10:
+		sec += 1
 	jump(delta)
 	walk(delta)
 	move_and_slide()
 	wall_slide(delta)
 	compute_particles()
+	land_sfx()
 	
 func walk(_delta):
 	var direction = Input.get_axis(no_move[0], no_move[1])
@@ -36,14 +39,16 @@ func walk(_delta):
 func jump(delta):
 	if Input.is_action_just_pressed("jump"):
 		buffer_frames = BUFFER_LENGTH
-		if Input.is_action_pressed("left") or Input.is_action_pressed("right") and !is_on_floor():
+		if (Input.is_action_pressed("left") or Input.is_action_pressed("right")) and !is_on_floor():
 			if $ray_right.is_colliding(): 
+				$jump_sfx.play()
 				velocity.y = JUMP_VELOCITY
 				velocity.x = -WALL_PUSH
 				no_move = ["left", "none"]
 				$move_timer.start()
 				reset_buffer()
 			if $ray_left.is_colliding(): 
+				$jump_sfx.play()
 				velocity.y = JUMP_VELOCITY
 				velocity.x = WALL_PUSH
 				no_move = ["none", "right"]
@@ -52,6 +57,7 @@ func jump(delta):
 	
 	if buffer_frames > 0:
 		if is_on_floor() or jumpReset == true:
+			$jump_sfx.play()
 			velocity.y = JUMP_VELOCITY
 
 	if Input.is_action_just_released("jump") and not is_on_floor():
@@ -82,13 +88,16 @@ func wall_slide(delta):
 func compute_particles():
 	if not is_on_floor() and $ray_right.is_colliding():
 		$ray_right/particles_right.emitting = true
-	else:
-		$ray_right/particles_right.emitting = false
-	
 	if not is_on_floor() and $ray_left.is_colliding():
 		$ray_left/particles_left.emitting = true
-	else:
-		$ray_left/particles_left.emitting = false
+		
+func land_sfx():
+	if !is_on_floor() and sec > 5:
+		next_land = true
+	if next_land == true and is_on_floor():
+		$AnimationPlayer.play("squish")
+		$land_sfx.play()
+		next_land = false
 
 func _on_move_timer_timeout():
 	no_move = ["left", "right"]
